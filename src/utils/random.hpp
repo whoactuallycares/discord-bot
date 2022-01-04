c35029562db03348a86f597737761f661ce7d982
Tommy <tomprochazka0@gmail.com>
2022-01-04 22:35:05 +0100

    feat: random number generator

--- src/utils/random.hpp
+++ src/utils/random.hpp
@@ -1,18 +1,15 @@
 #pragma once
 #include <random>
 
-class random
+namespace rand
 {
-public:
-	static int64_t randint(int64_t _min, int64_t _max)
-	{
-		std::uniform_int_distribution<int> uni(_min, _max);
-		return uni(_rng);
-		auto random_integer = uni(_rng);
-	};
-	static std::random_device _rd;
-	static std::mt19937 _rng;
-};
+	std::random_device rd;
+	std::mt19937 rng = std::mt19937(rd());
 
-std::random_device random::_rd;
-std::mt19937 random::_rng = std::mt19937(_rd());
+	template<typename T>
+	T rand(T _min, T _max)
+	{
+		std::uniform_int_distribution<T> uni(_min, _max);
+		return uni(rng);
+	};
+};
