d3dfe95f97fe26be4ceeea4793a817b8b0fab64f
Tommy <tomprochazka0@gmail.com>
2022-01-17 23:17:15 +0100

    feat: per_guild implementation

--- src/per_guild.hpp
+++ src/per_guild.hpp
@@ -9,17 +9,27 @@ template<typename T>
 class per_guild
 {
 public:
-	per_guild();
+	per_guild() = default;
 	per_guild(const per_guild&) = delete;
 	per_guild& operator=(const per_guild&) = delete;
 
-	T& operator[](dpp::snowflake _guildId)
+	inline T& operator[](dpp::snowflake _guildId)
 	{
+		if (map_.find(_guildId) == map_.end())
+		{
+			map_.insert({_guildId, data_.size()});
+			data_.emplace_back();
+		};
 		return data_[map_[_guildId]];
 	};
 
-	void deserialize(std::string _path);
-	void serialize();
+	void deserialize()
+	{
+	};
+	void serialize()
+	{
+		// save the data into "data/guildData"
+	};
 private:
 	std::map<dpp::snowflake, uint32_t> map_; // Map guildId to an index in data_ vector
 	std::vector<T> data_;
